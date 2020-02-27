fc9785d1bcb461da8076d743510179b0d62ff0e4
Majkl <majkl.andracic@gmail.com>
2020-02-27 23:13:36 +0100

    Dodatak kava

--- app/src/main/java/com/example/kuca_kave/Drink.java
+++ app/src/main/java/com/example/kuca_kave/Drink.java
@@ -1,4 +1,14 @@
 package com.example.kuca_kave;
 
 public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    pubic static final Drink[] drinks = {
+            new Drink("Kava s mlijekom", "Espresso sa toplim ili hladnim mlijekom", R.drawable.latte),
+            new Drink("Cappucino", "Espresso sa pjenom od mlijeka", R.drawable.cappucino),
+            new Drink("Espresso", "Kratka crna kava", R.drawable.espresso)
+    };
+
 }
