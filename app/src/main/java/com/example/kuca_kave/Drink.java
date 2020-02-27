4d76a2c6a0ef7d941fdf76017a348fb823a878bd
Majkl <majkl.andracic@gmail.com>
2020-02-27 23:16:20 +0100

    Konstruktor i geteri

--- app/src/main/java/com/example/kuca_kave/Drink.java
+++ app/src/main/java/com/example/kuca_kave/Drink.java
@@ -11,4 +11,27 @@ public class Drink {
             new Drink("Espresso", "Kratka crna kava", R.drawable.espresso)
     };
 
+    public Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
 }
