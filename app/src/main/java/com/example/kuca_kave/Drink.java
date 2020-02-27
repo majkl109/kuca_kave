25bf18d04b3de607a970ef4d790e998d74a4e61d
Majkl <majkl.andracic@gmail.com>
2020-02-27 23:53:18 +0100

    Item click listener

--- app/src/main/java/com/example/kuca_kave/Drink.java
+++ app/src/main/java/com/example/kuca_kave/Drink.java
@@ -5,7 +5,7 @@ public class Drink {
     private String description;
     private int imageResourceId;
 
-    pubic static final Drink[] drinks = {
+    public static final Drink[] drinks = {
             new Drink("Kava s mlijekom", "Espresso sa toplim ili hladnim mlijekom", R.drawable.latte),
             new Drink("Cappucino", "Espresso sa pjenom od mlijeka", R.drawable.cappucino),
             new Drink("Espresso", "Kratka crna kava", R.drawable.espresso)
--- app/src/main/java/com/example/kuca_kave/TopLevelActivity.java
+++ app/src/main/java/com/example/kuca_kave/TopLevelActivity.java
@@ -2,7 +2,10 @@ package com.example.kuca_kave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+    }
+
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            if(position == 0){
+                Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
