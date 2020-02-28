cfbc67c03cc2fc25951a1bc1a155c1cdf28385d3
Majkl <majkl.andracic@gmail.com>
2020-02-28 21:33:33 +0100

    onItemClickListener

--- app/src/main/java/com/example/kuca_kave/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/kuca_kave/DrinkCategoryActivity.java
@@ -2,7 +2,10 @@ package com.example.kuca_kave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,5 +21,14 @@ public class DrinkCategoryActivity extends AppCompatActivity {
         ListView listDrinks =  findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
 
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listDrinks, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID,(int) id);
+                startActivity(intent);
+            }
+        };
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
