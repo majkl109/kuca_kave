bfc91ce66eaffb80473535c5c05d431c1dd929dd
Majkl <majkl.andracic@gmail.com>
2020-03-14 21:13:37 +0100

    FoodCategory activity created

---
+++ app/src/main/java/com/example/kuca_kave/FoodCategoryActivity.java
@@ -0,0 +1,38 @@
+package com.example.kuca_kave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class FoodCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_category);
+        ArrayAdapter<Food> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_2,
+                Food.foods);
+        ListView listFoods =  findViewById(R.id.list_foods);
+        listFoods.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listFoods, View view, int position, long id) {
+                Intent intent = new Intent(FoodCategoryActivity.this,FoodActivity.class);
+                intent.putExtra(FoodActivity.EXTRA_FOOD_ID,(int) id);
+                startActivity(intent);
+            }
+        };
+        listFoods.setOnItemClickListener(itemClickListener);
+    }
+}
+
+    }
+
+}
