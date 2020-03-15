787ea6130b2f9351ce547cbbec50c9e8e86a0302
Majkl <majkl.andracic@gmail.com>
2020-03-15 23:39:09 +0100

    Created Store class

--- app/src/main/java/com/example/kuca_kave/Store.java
+++ app/src/main/java/com/example/kuca_kave/Store.java
@@ -1,4 +1,38 @@
 package com.example.kuca_kave;
 
 public class Store {
+
+    private String coffeeName;
+    private String description;
+    private int  imageResourceID;
+
+    public static final Coffee[] coffees = {
+
+            new Coffee("Brazil", "Brazilska kava", R.drawable.brazil),
+            new Coffee("Columbia", "Kolumbjska kava", R.drawable.colombia),
+            new Coffee("Africa", "Afrička kava iz više država", R.drawable.africa)
+    };
+
+    public Store(String coffeeName, String description, int imageResourceID) {
+        this.coffeeName = coffeeName;
+        this.description = description;
+        this.imageResourceID = imageResourceID;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    @Override
+    public String toString() {
+        return this.coffeeName;
+    }
 }
